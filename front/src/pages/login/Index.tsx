a1c3635bcb9133edce2ff12ae0e3387375ae3b99
Kate0418 <nakao.keito0418@gmail.com>
2024-08-03 22:33:12 +0900

    ディレクトリー構造をおまかに作成

---
+++ front/src/pages/login/Index.tsx
@@ -0,0 +1,13 @@
+const Index = () => {
+    return (
+        <>
+            <div>
+                <p className="text-red-500">
+                    ログインページ
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Index
--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // RouterProvider を追加
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './tailwind.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginIndex from './pages/login/Index'
 
 export const router = createBrowserRouter([
     {
-        path: "/app",
-        element: <App />,
+        path: "/login",
+        element: <LoginIndex />,
+        children:[]
     },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
