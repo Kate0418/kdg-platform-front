37164b44b1c870fc91fa5ccc4611d9b96af9de97
Kate0418 <nakao.keito0418@gmail.com>
2024-08-04 01:05:26 +0900

    ログインフォームのベース完成

--- front/src/components/login/index/Form.tsx
+++ front/src/components/login/index/Form.tsx
@@ -1,9 +1,54 @@
+import React, { useState } from 'react'
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
 const Form = () => {
-    return (
-        <>
-            <p className="text-red-800">ログインフォーム</p>
-        </>
-    )
+
+    const [password, setPassword] = useState("");
+    const [passwordType, setPasswordType] = useState("password");
+
+  return (
+    <div className='Form'>
+
+        <form>
+            <div className='form_whole'>
+                <h1>ログイン</h1>
+                <hr />
+
+                <div className='form_part'>
+                    <label>ログインID</label>
+                    <input type='text' placeholder='ログインID' name='login_id'/>
+                </div>
+                <div className='form_part'>
+                    <label>パスワード</label>
+                    <input
+                        value={password}
+                        type={passwordType}
+                        placeholder='パスワード'
+                        name='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {passwordType === "password" && (
+                        <VisibilityOffIcon
+                            onClick={() => setPasswordType("text")}
+                            className="Password__visual"
+                        />
+                    )}
+                    {passwordType === "text" && (
+                        <VisibilityIcon
+                            onClick={() => setPasswordType("password")}
+                            className="Password__visual"
+                        />
+                    )}
+                </div>
+
+                <button>ログイン</button>
+
+            </div>
+        </form>
+
+    </div>
+  )
 }
 
 export default Form
