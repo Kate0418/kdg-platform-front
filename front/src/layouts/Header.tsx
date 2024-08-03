73f00fb171dcb22e49fba0fb5ffb32afef085873
Kate0418 <nakao.keito0418@gmail.com>
2024-08-04 00:58:13 +0900

    ヘッダー完成

--- front/src/layouts/Header.tsx
+++ front/src/layouts/Header.tsx
@@ -2,8 +2,9 @@ const Header = () => {
     return (
         <>
             <header>
-                <div className="p-10">
-                    <img className='w-1/4' src='/imgs/logo.gif'></img>
+                <div className="p-3 bg-[#B4BD4F]">
+                    <img className='w-1/4 p-1' src='/imgs/logo.gif'></img>
+                    <div className="border-b-2 border-[#6A5E57]"></div>
                 </div>
             </header>
         </>
