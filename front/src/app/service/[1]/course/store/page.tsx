babf304b5735be3c04ad8363c4bc2d6b538c2607
Kate0418 <nakao.keito0418@gmail.com>
2024-11-05 00:36:17 +0900

    コース登録モーダル

--- front/src/app/service/[1]/course/store/page.tsx
+++ front/src/app/service/[1]/course/store/page.tsx
@@ -14,6 +14,7 @@ import { daysOfWeek, SelectItem } from "@/config";
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Modal } from "@/components/layout/Modal";
 
 interface Course {
   name: string | null;
@@ -39,6 +40,7 @@ export default function Page() {
       data: null,
     })),
   });
+  const [modalFlg, setModalFlg] = useState(false);
 
   useEffect(() => {
     const selectApi = async () => {
@@ -74,7 +76,10 @@ export default function Page() {
       <List title="登録コース" h={520}>
         <div className="flex items-center pl-2 pb-2">
           <label>コース名：</label>
-          <input className="p-1 border border-[var(--text-color)] rounded-lg" />
+          <input
+            className="p-1 border border-[var(--text-color)] rounded-lg"
+            onChange={(e) => setCourse({ ...course, name: e.target.value })}
+          />
           <label className="pl-2">学年：</label>
           <Select
             className="w-32"
@@ -98,8 +103,22 @@ export default function Page() {
                   className="flex flex-col justify-center items-center border-b border-[var(--text-color)] h-20"
                 >
                   <div className="w-full px-1">
-                    <input type="time" />
-                    <input type="time" />
+                    <input
+                      type="time"
+                      onChange={(e) => {
+                        const newCourse = { ...course };
+                        newCourse.times[j].start = e.target.value;
+                        setCourse(newCourse);
+                      }}
+                    />
+                    <input
+                      type="time"
+                      onChange={(e) => {
+                        const newCourse = { ...course };
+                        newCourse.times[j].end = e.target.value;
+                        setCourse(newCourse);
+                      }}
+                    />
                   </div>
                 </div>
               ))}
@@ -193,12 +212,85 @@ export default function Page() {
         </DndContext>
       </List>
       <div className="flex justify-between w-full">
-        <A href="/service/1/course">戻る</A>
+        <A href="/service/1/course">キャンセル</A>
 
         <div>
-          <Button type="button">確認</Button>
+          <Button
+            type="button"
+            onClick={() => {
+              setModalFlg(true);
+            }}
+          >
+            確認
+          </Button>
         </div>
       </div>
+
+      <Modal modalFlg={modalFlg} setModalFlg={setModalFlg}>
+        <div className="w-full pl-2 pb-2">
+          コース名: {course.name}
+          <br />
+          学年: {grades.find((grade) => grade.value === course.grade_id)?.label}
+        </div>
+
+        <div className="w-full grid lg:grid-cols-[1fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] gap-4">
+          <ScheduleColumn head="時間">
+            {Array.from({ length: 12 }).map((_, j) => (
+              <div
+                key={j}
+                className="flex flex-col justify-center items-center border-b border-[var(--text-color)] h-20"
+              >
+                {course.times[j].start && course.times[j].end && (
+                  <div className="w-full px-1 text-center">
+                    {course.times[j].start}
+                    <br />
+                    ︙
+                    <br />
+                    {course.times[j].end}
+                  </div>
+                )}
+              </div>
+            ))}
+          </ScheduleColumn>
+          {Array.from({ length: 7 }).map((_, i) => (
+            <ScheduleColumn head={daysOfWeek[i]} key={i}>
+              {Array.from({ length: 12 }).map((_, j) => (
+                <div
+                  key={j}
+                  className={`flex flex-col justify-center items-center border-b border-[var(--text-color)] h-20
+                    ${
+                      (!course.lessons[i * 12 + j].action ||
+                        !course.lessons[i * 12 + j].data) &&
+                      "bg-[var(--text-color-60)]"
+                    }`}
+                >
+                  <div className="w-full p-2 flex">
+                    {
+                      subjects.find(
+                        (subject) =>
+                          subject.value === course.lessons[i * 12 + j].data,
+                      )?.label
+                    }
+                  </div>
+                </div>
+              ))}
+            </ScheduleColumn>
+          ))}
+        </div>
+        <div className="flex justify-end w-full">
+          <Button
+            type="button"
+            onClick={() => {
+              setModalFlg(false);
+            }}
+          >
+            戻る
+          </Button>
+          <Button type="button" onClick={() => {}}>
+            登録
+          </Button>
+        </div>
+      </Modal>
     </>
   );
 }
--- front/src/components/layout/Modal.tsx
+++ front/src/components/layout/Modal.tsx
@@ -1,16 +1,24 @@
 interface Props {
-    className?: string
-    modalFlg: boolean
-    children?: React.ReactNode
+  className?: string;
+  modalFlg: boolean;
+  setModalFlg: React.Dispatch<React.SetStateAction<boolean>>;
+  children?: React.ReactNode;
 }
 
-export function Modal ({ className, modalFlg, children }: Props) {
-    return (
-        <div className={`fixed inset-0 z-40 flex items-center justify-center ${modalFlg || 'hidden'}`}>
-            <div className="absolute inset-0 bg-[var(--text-color)] opacity-60"></div>
-            <div className={`relative z-50 bg-white p-4 lg:p-8 rounded-lg w-full h-[600px] overflow-auto mx-8 lg:mx-20 flex flex-col items-center ${className}`}>
-                {children}
-            </div>
-        </div>    
-    )
+export function Modal({ className, modalFlg, setModalFlg, children }: Props) {
+  return (
+    <div
+      className={`fixed inset-0 z-40 flex items-center justify-center ${modalFlg || "hidden"}`}
+    >
+      <div
+        className="absolute inset-0 bg-[var(--text-color)] opacity-60"
+        onClick={() => setModalFlg(false)}
+      ></div>
+      <div
+        className={`relative z-50 bg-white p-4 lg:p-8 rounded-lg w-full h-[600px] overflow-auto mx-8 lg:mx-20 flex flex-col items-center ${className}`}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
